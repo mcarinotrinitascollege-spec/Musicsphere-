1721562d617af73067c5b27507a94f43304de72f
mcarinotrinitascollege-spec <mcarino.trinitascollege@gmail.com>
2026-03-08 17:22:17 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,10 +1,25 @@
-const CACHE_NAME = 'musicsphere-v1';
-const ASSETS = ['./', './index.html', './manifest.json'];
+const CACHE_NAME = 'musicsphere-v2'; // Increment this number whenever you update!
 
-self.addEventListener('install', (e) => {
-  e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
+self.addEventListener('install', (event) => {
+  self.skipWaiting(); // Forces the waiting Service Worker to become active
 });
 
-self.addEventListener('fetch', (e) => {
-  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cache) => {
+          if (cache !== CACHE_NAME) {
+            return caches.delete(cache); // Deletes old versions
+          }
+        })
+      );
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    fetch(event.request).catch(() => caches.match(event.request))
+  );
 });
